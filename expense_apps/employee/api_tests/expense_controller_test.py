88fdaf99fc9d718cb2d6e4e7f695c27d32508770
Ian Barr <ian.barr.tx@gmail.com>
2025-12-15 15:34:35 -0600

    structure: api unit test files structured

---
+++ expense_apps/employee/api_tests/expense_controller_test.py
@@ -0,0 +1,19 @@
+import pytest
+
+class Test_Expense_Controller():
+
+    def test_submit_expense(self):
+        pass
+
+    def test_get_expenses_for_user(self):
+        pass
+
+    def test_get_expense_for_id(self):
+        pass
+
+    def test_update_expense_by_id(self):
+        pass
+
+    def test_delete_expense_by_id(self):
+        pass
+    
