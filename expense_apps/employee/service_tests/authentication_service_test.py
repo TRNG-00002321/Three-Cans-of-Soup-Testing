057029e1ee743efada87340b6481fcca152e2bbf
Ian Barr <ian.barr.tx@gmail.com>
2025-12-15 16:13:30 -0600

    structure: added empty tests to service

---
+++ expense_apps/employee/service_tests/authentication_service_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+class Test_AuthenticationService():
+
+    def test_authenticate_user(self):
+        pass
+
+    def test_get_user_by_id(self):
+        pass
+
+    def test_jwt_token_generation(self):
+        pass
+
+    def test_jwt_token_validation(self):
+        pass
+
+    def test_get_user_by_token(self):
+        pass
---
+++ expense_apps/employee/service_tests/expense_service__test.py
@@ -0,0 +1,24 @@
+import pytest
+
+class Test_ExpenseService():
+
+    def test_submit_expense(self):
+        pass
+
+    def test_get_user_expenses_with_status(self):
+        pass
+
+    def test_get_expense_by_id(self):
+        pass
+
+    def test_get_expense_by_status(self):
+        pass
+
+    def test_update_expense(self):
+        pass
+
+    def test_delete_expense(self):
+        pass
+
+    def test_get_expense_history(self):
+        pass
