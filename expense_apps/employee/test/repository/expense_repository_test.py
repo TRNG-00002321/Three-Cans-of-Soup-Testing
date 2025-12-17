2c774d320417cd758bbefed986929ee061375ede
Ian Barr <ian.barr.tx@gmail.com>
2025-12-17 13:42:13 -0600

    Expense Repository delete valid-id and invalid-id

---
+++ expense_apps/employee/test/repository/expense_repository_test.py
@@ -0,0 +1,27 @@
+"""
+Unit tests for ExpenseRepository Class
+"""
+import pytest
+from repository import ExpenseRepository, DatabaseConnection
+
+
+class TestExpenseRepository():
+        
+
+    def test_delete_valid_id_returns_true(self, expense_repository, conn, cursor):
+        cursor.rowcount = 1
+
+        deleted = expense_repository.delete(0)
+
+        assert conn.execute.call_count == 2
+        conn.commit.assert_called_once()
+        assert deleted
+    
+    def test_delete_invalid_id_returns_true(self, expense_repository, conn, cursor):
+        cursor.rowcount = 0
+
+        deleted = expense_repository.delete(0)
+
+        assert conn.execute.call_count == 2
+        conn.commit.assert_called_once()
+        assert not deleted
