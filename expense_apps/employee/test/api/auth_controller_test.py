648dfd987b64f6328781f2c92ca941e05c98510c
SunniM <sunnismagan@gmail.com>
2025-12-23 17:32:09 -0600

    feat(EMS-36) moved auth pytest fixtures to api/conftest.py

--- expense_apps/employee/test/api/auth_controller_test.py
+++ expense_apps/employee/test/api/auth_controller_test.py
@@ -2,33 +2,12 @@ import pytest
 from unittest.mock import Mock, MagicMock
 from flask import Flask
 
-@pytest.fixture
-def app():
-    app = Flask(__name__)
-
-    app.auth_service = MagicMock()
-    from api.auth_controller import auth_bp
-    app.register_blueprint(auth_bp)
-    return app
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-@pytest.fixture
-def mock_user():
-    user = Mock()
-    user.id = 1
-    user.username = 'employee1'
-    user.password = 'password123'
-    user.role = 'employee'
-    return user
 
 
 class Test_Auth_Controller():
-    def test_login_valid_user(self, client, app, mock_user):
+    def test_login_valid_user(self, client, app, mock_employee):
         # Arrange
-        app.auth_service.authenticate_user.return_value = mock_user
+        app.auth_service.authenticate_user.return_value = mock_employee
         app.auth_service.generate_jwt_token.return_value = "fake-token"
 
         # Act
@@ -55,16 +34,9 @@ class Test_Auth_Controller():
         assert 401 == response.status_code
         app.auth_service.authenticate_user.assert_called_once()
         assert response.json['error'] == 'Invalid credentials'
-
-    def test_login_no_body(self, client):
-        # Act
-        response = client.post("api/auth/login", json='')
-
-       # Assert
-        assert response.status_code == 400
-        assert response.json['error'] == 'JSON data required'
         
     @pytest.mark.parametrize("json_data, error_message", [
+        ('', 'JSON data required'),
         ({}, 'JSON data required'),
         ({'password' : 'password123'}, 'Username and password required'),
         ({'username' : 'employee1'}, 'Username and password required'),
@@ -84,9 +56,9 @@ class Test_Auth_Controller():
         print(response.headers['Set-Cookie'])
         assert 'Expires=Thu, 01 Jan 1970 00:00:00 GMT' in response.headers['Set-Cookie']
 
-    def test_status(self, client, app, mock_user):
+    def test_status_authenicated(self, client, app, mock_employee):
         client.set_cookie('jwt_token', 'fake_token')
-        app.auth_service.get_user_from_token.return_value = mock_user
+        app.auth_service.get_user_from_token.return_value = mock_employee
 
         response = client.get("api/auth/status")
         # Assert
@@ -95,13 +67,20 @@ class Test_Auth_Controller():
         assert response.json['user']['id'] == 1
         assert response.json['user']['username'] == 'employee1'
         assert response.json['user']['role'] == 'employee'
-
-
-    def test_status_server_error(self, client, app):
-        # Arrange: Force the service to raise an exception
-        app.auth_service.get_user_from_token.side_effect = Exception("Authentication failed")
         
-        # We need a cookie to get past the first 'if not token' check
+    def test_status_not_authenicated(self, client, app, mock_employee):
+
+        response = client.get("api/auth/status")
+        # Assert
+        assert response.status_code == 200
+        assert not response.json['authenticated']
+
+
+    #TODO: Log Defect in Jira
+    @pytest.mark.skip("Incorrect Status Code - JIRA: EMS-42 ")
+    def test_status_server_error(self, client, app):
+        # Arrange
+        app.auth_service.get_user_from_token.side_effect = Exception("Authentication failed")
         client.set_cookie('jwt_token', 'fake_token')
 
         # Act
@@ -110,4 +89,4 @@ class Test_Auth_Controller():
         # Assert
         assert response.status_code == 500
         assert response.json['error'] == 'Internal server error'
-        assert 'Database connection failed' in response.json['details']
+        assert 'Database connection failed' in response.json['error']
